c3b829062a7575ba18dab8e77698ea3eec2c7b39
Omar Ashour <omar.ashour22@gmail.com>
2023-07-19 22:56:49 -0700

    Complete DOS implementation

--- pymatgen/io/espresso/outputs/dos.py
+++ pymatgen/io/espresso/outputs/dos.py
@@ -2,70 +2,157 @@
 Classes for reading/manipulating projwfc.x/dos.x DOS files
 """
 
-from __future__ import annotations
-
 import itertools
 import re
-import os
-import xml.etree.ElementTree as ET
 
 import numpy as np
 import glob
 from monty.json import MSONable
-import pandas as pd
-from tabulate import tabulate
 
-from pymatgen.core.lattice import Lattice
-from pymatgen.core.structure import Structure, Site
-from pymatgen.core.units import (
-    Ry_to_eV,
-    bohr_to_ang,
-)
+from pymatgen.core.structure import Site
+from pymatgen.electronic_structure.core import OrbitalType, Spin
 
-from pymatgen.electronic_structure.core import Magmom, Orbital, OrbitalType, Spin
 from pymatgen.io.espresso.outputs.projwfc import AtomicState
-from pymatgen.io.espresso.utils import parse_pwvals, ibrav_to_lattice, projwfc_orbital_to_vasp
 
 
 class Dos(MSONable):
     """
-    Class for representing DOS data from a PWscf dos.x or projwfc.x calculation
+    Class for representing DOS data from a dos.x or projwfc.x calculation
     """
 
-    def __init__(self, fildos, noncolinear=False):
+    def __init__(
+        self,
+        energies,
+        tdos,
+        idos=None,
+        ldos=None,
+        atomic_states=None,
+        Efermi=None,
+        lsda=None,
+        noncolinear=None,
+        lspinorb=None,
+    ):
         """
         Args:
-            fildos (str): filepath to the dos file. Note that this is
-                the same as in dos.x/projwfc.x input, so it shouldn't include the rest
-                of the filename. For example, fildos="path/to/fildos" will look for
-                "path/to/fildos.pdos_atm#_wfc#..." type of files
-            noncolinear (bool): Whether the calculation is noncolinear WITHOUT SOC.
+            energies (np.ndarray): energies in eV
+            tdos (Mapping[Spin, ArrayLike]): total DOS
+            idos (Mapping[Spin, ArrayLike]): integrated DOS
+            ldos (Mapping[Spin, ArrayLike]): local DOS for each wave function
+                                             (Not lm or ljmj decomposed)
+            atomic_states (list[AtomicState]): list of AtomicState objects
+            Efermi (float): Fermi energy
+            lsda (bool | None): Whether the calculation is spin polarized.
+                                None indicates unknown.
+            noncolinear (bool | None): Whether the calculation is noncolinear
+                                       (with or without SOC). None indicates unknown
+            lspinorb (bool | None): Whether the calculation is spin orbit coupled
+                                    None indicates unknown.
+        """
+
+        self.energies = energies
+        self.tdos = tdos
+        self.idos = idos
+        self.ldos = ldos
+        self.atomic_states = atomic_states
+        self.Efermi = Efermi
+        self.noncolinear = noncolinear
+        self.lsda = lsda
+        self.lspinorb = lspinorb
+
+    @classmethod
+    def from_filpdos(cls, filpdos):
+        """
+        Args:
+            filpdos (str): filepath to the pdos file. Note that this is
+                the same as in projwfc.x input, so it shouldn't include the rest
+                of the filename. For example, filpdos="path/to/filpdos" will look for
+                "path/to/filpdos.pdos_atm#_wfc#..." type of files
+
+            Whether the calculation is noncolinear WITHOUT SOC.
                                 projwfc.x's output does not distinguish between
                                 noncolinear without SOC and spin polarized calculations.
                                 If False, the calculation is assumed to be spin polarized
                                 (if the DOS file format leaves any ambiguity)
         """
-        self.fildos = fildos
-        self.atomic_states = []
-        all_energies = []
 
-        filenames = glob.glob(f"{fildos}.pdos_atm*")
+        # Read the total DOS first
+        # The only way to differentiate between spin polarized and noncolinear without SOC
+        # is to check the number of columns in the fildos.pdos_tot file
+        E, tdos, _, ncl_no_soc, lsda = cls._read_total_pdos(f"{filpdos}.pdos_tot")
+        all_energies = [E]
+
+        atomic_states = []
+        ldos = []
+        filenames = glob.glob(f"{filpdos}.pdos_atm*")
         for f in filenames:
-            states, E = self._get_atomic_states(f, noncolinear)
-            self.atomic_states.extend(states)
+            E, ld, states = cls._read_pdos(f, ncl_no_soc)
+            atomic_states.extend(states)
+            ldos.append(ld)
             all_energies.append(E)
 
         if not np.allclose(all_energies, all_energies[0], rtol=0, atol=1e-4):
-            raise ValueError("Energies from all files do not match")
-        self.energies = all_energies[0]
-        self.order_states()
+            raise ValueError("Energies from all files do not match.")
+        energies = all_energies[0]
+        # Order the atomic states and compute the state #
+        atomic_states = cls._order_states(atomic_states)
+        lspinorb = atomic_states[0].j is not None
+        noncolinear = ncl_no_soc or lspinorb
 
-    def order_states(self):
+        return cls(
+            energies,
+            tdos,
+            ldos=ldos,
+            atomic_states=atomic_states,
+            noncolinear=noncolinear,
+            lsda=lsda,
+            lspinorb=lspinorb,
+        )
+
+    @classmethod
+    def from_fildos(cls, fildos):
+        """
+        Constructs a Dos object from a fildos (dos.x) file
+
+        The format of Fildos is as follows (TDOS = total DOS, IDOS = integrated DOS):
+        * Spin polarized:
+            Energy(ev) TDOS(up) TODS(dn) IDOS
+        * Everything else:
+            Energy(ev) TDOS IDOS
+
+        Args:
+            filpdos (str): path to the dos file. Same as the dos.x input.
+        """
+        with open(fildos, "r") as f:
+            header = f.readline()
+            if match := re.match(".*?EFermi\s*=\s*(\d+\.\d+)\s*eV.*?", header):
+                Efermi = float(match[1])
+            else:
+                raise ValueError("Cannot find Fermi energy in the header.")
+
+        lsda = False
+
+        data = np.loadtxt(fildos, skiprows=1)
+        energies = data[:, 0]
+        if (ncols := data.shape[1]) == 3:
+            # Colinear or noncolinear (with or without SOC)
+            tdos, idos = {Spin.up: data[:, 1]}, data[:, 2]
+        elif ncols == 4:
+            # spin polarized
+            lsda = True
+            tdos = {Spin.up: data[:, 1], Spin.down: data[:, 2]}
+            idos = data[:, 3]
+        else:
+            raise ValueError(f"Unexpected number of columns {ncols} in {fildos}")
+
+        return cls(energies, tdos, idos=idos, Efermi=Efermi, lsda=lsda)
+
+    @staticmethod
+    def _order_states(atomic_states):
         """
         Sets the index (state #) of the states in the same order as the projwfc.x output
         """
-        noncolinear = self.atomic_states[0].s_z is not None
-        lspinorb = self.atomic_states[0].j is not None
+        noncolinear = atomic_states[0].s_z is not None
+        lspinorb = atomic_states[0].j is not None
 
         if lspinorb:
             sort_order = lambda x: (x.site.atom_i, x.wfc_i, x.l, x.j, x.mj)
@@ -74,12 +161,58 @@ class Dos(MSONable):
         else:
             sort_order = lambda x: (x.site.atom_i, x.wfc_i, x.l, x.m)
 
-        self.atomic_states = sorted(self.atomic_states, key=sort_order)
-        for i, state in enumerate(self.atomic_states):
+        atomic_states = sorted(atomic_states, key=sort_order)
+        for i, state in enumerate(atomic_states):
             state.state_i = i + 1
 
+        return atomic_states
+
     @staticmethod
-    def _get_atomic_states(filename, noncolinear):
+    def _read_total_pdos(filename):
+        """
+        Reads a filpdos.pdos_tot
+        Returns the energies, total DOS, and summed PDOS
+
+
+
+        * Colinear and noncolinear with SOC:
+            Energy(eV) TDOS \sum_lm(PDOS_lm)
+        * Colinear spin polarized:
+            Energy(eV) TDOS(up) TDOS(dn) \sum_lm(PDOS_lm(up)) \sum_lm(PDOS_lm(dn))
+        * Noncolinear without SOC:
+            Energy(eV) TDOS \sum_lm(PDOS_lm(up)) \sum_lm(PDOS_lm(dn))
+
+        Returns:
+            energies (np.ndarray): energies in eV
+            tdos     (Mapping[Spin, ArrayLike]): total DOS
+            sum_pdos (Mapping[Spin, ArrayLike]): summed PDOS
+            ncl_no_soc (bool): Whether the calculation is noncolinear *without* SOC
+        """
+
+        ncl_no_soc = False
+        lsda = False
+
+        data = np.loadtxt(filename, skiprows=1)
+        energies = data[:, 0]
+        if (ncols := data.shape[1]) == 3:
+            # Colinear or noncolinear with SOC
+            tdos, sum_pdos = {Spin.up: data[:, 1]}, {Spin.up: data[:, 2]}
+        elif ncols == 4:
+            # Noncolinear without SOC
+            ncl_no_soc = True
+            tdos = {Spin.up: data[:, 1]}
+            sum_pdos = {Spin.up: data[:, 2], Spin.down: data[:, 3]}
+        elif ncols == 5:
+            # Colinear spin polarized
+            lsda = True
+            tdos = ({Spin.up: data[:, 1], Spin.down: data[:, 2]},)
+            sum_pdos = {Spin.up: data[:, 3], Spin.down: data[:, 4]}
+        else:
+            raise ValueError(f"Unexpected number of columns {ncols} in {filename}")
+        return energies, tdos, sum_pdos, ncl_no_soc, lsda
+
+    @staticmethod
+    def _read_pdos(filename, ncl_no_soc):
         """
         Gets an AtomicState object for each atomic state in the file
 
@@ -91,9 +224,12 @@ class Dos(MSONable):
                 Colinear spin pol or noncolinear no SOC: 2 for LDOS (u/d), 2 for ea PDOS_m (u/d)
                 Noncolinear with SOC: 1 for LDOS, 1 for each PDOS_mj
             LDOS = \sum_m PDOS_m or \sum_mj PDOS_mj
+            The LDOS column should in principle be the same as the sum of the PDOS columns
+            but finite precision may cause them to differ slightly
 
         params:
             filename (str): filename of the pdos file
+            ncl_no_soc (bool): Whether the calculation is noncolinear without SOC
         returns:
             list of AtomicState objects
         """
@@ -110,13 +246,15 @@ class Dos(MSONable):
         energies = data[:, 0]
         if (ncols := data.shape[1]) == (2 + 2 * l + 1) and j is None:
             # Colinear case
+            ldos = {Spin.up: data[:, 1]}
             pdos = [{Spin.up: p} for p in data[:, 2:].T]
             params = [
                 {"site": site, "wfc_i": wfc_i, "l": l, "m": m} for m in np.arange(1, 2 * l + 2)
             ]
         elif ncols == (1 + 2 * (1 + 2 * l + 1)) and j is None:
             # Colinear spin polarized or noncolinear without SOC
-            if noncolinear:
+            ldos = {Spin.up: data[:, 1], Spin.down: data[:, 2]}
+            if ncl_no_soc:
                 pdos = [{Spin.up: p} for p in data[:, 3:].T]
                 params = [
                     {"site": site, "wfc_i": wfc_i, "l": l, "m": m, "s_z": s_z}
@@ -132,13 +270,17 @@ class Dos(MSONable):
         elif ncols == (2 + 2 * j + 1) and j is not None:
             # Noncolinear with SOC
             pdos = [{Spin.up: p} for p in data[:, 2:].T]
+            ldos = {Spin.up: data[:, 1]}
             params = [
                 {"site": site, "wfc_i": wfc_i, "l": l, "j": j, "mj": mj}
                 for mj in np.arange(-j, j + 1)
             ]
         else:
-            raise ValueError(f"Unexpected number of columns in {filename}")
+            raise ValueError(
+                f"Unexpected number of columns in {filename}. "
+                "Are you trying to read k-resolved DOS? It's currently not implemented."
+            )
 
         atomic_states = [AtomicState(pm, pdos=pd) for pm, pd in zip(params, pdos)]
 
-        return atomic_states, energies
+        return energies, ldos, atomic_states
