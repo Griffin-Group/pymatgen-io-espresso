bugfix: _parse_vbm_cbm_efermi returned vbm, cbm in wrong order

tests/test_pwxml.py
@@ -27,11 +27,14 @@ def test_init(mat: str) -> None:
         "MgO Gamma-point calculation",
         mat="MgO_gamma",
         final_energy_eV=-8158.01764759 * Ry_to_eV,
+        efermi_eV=5.3759,
     )
 )
-def test_parsing(mat: str, final_energy_eV: float) -> None:
+def test_parsing(mat: str, final_energy_eV: float, efermi_eV: float) -> None:
     """Tests that a pwscf.xml is parsed correctly."""
 
     pwxml = PWxml(f"tests/data/{mat}/pwscf.xml")
     assert np.allclose(pwxml.final_energy, final_energy_eV)
     assert np.allclose(pwxml.ionic_steps[-1]["total_energy"]["etot"], final_energy_eV)
+    assert np.allclose(pwxml.efermi, efermi_eV)
+    assert np.allclose(pwxml.vbm, efermi_eV)