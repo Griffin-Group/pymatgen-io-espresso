bugfix+tests: removed unneeded unit conversion from final_energy property

tests/test_pwxml.py
@@ -1,5 +1,9 @@
+"""Tests for PWXml class."""
+
+import numpy as np
 from pytest_parametrize_cases import Case, parametrize_cases
 
+from pymatgen.core.units import Ry_to_eV
 from pymatgen.io.espresso.outputs import PWxml
 
 
@@ -13,9 +17,21 @@ from pymatgen.io.espresso.outputs import PWxml
         mat="ZnO",
     ),
 )
-def test_init(mat: str):
-    """
-    Tests the equality dunder method of Projwfc
-    (implicitly also tests the __eq__ method of AtomicState)
-    """
+def test_init(mat: str) -> None:
+    """Tests that pwscf.xml initializes without raising an exception."""
     _ = PWxml(f"tests/data/{mat}/pwscf.xml")
+
+
+@parametrize_cases(
+    Case(
+        "MgO Gamma-point calculation",
+        mat="MgO_gamma",
+        final_energy_eV=-8158.01764759 * Ry_to_eV,
+    )
+)
+def test_parsing(mat: str, final_energy_eV: float) -> None:
+    """Tests that a pwscf.xml is parsed correctly."""
+
+    pwxml = PWxml(f"tests/data/{mat}/pwscf.xml")
+    assert np.allclose(pwxml.final_energy, final_energy_eV)
+    assert np.allclose(pwxml.ionic_steps[-1]["total_energy"]["etot"], final_energy_eV)